47ae7376925c776af2a977987effb1ee7072b1c3
alexshevvv <alekseyshev5@gmail.com>
2024-09-09 11:14:13 +0300

    Initial Commit

---
+++ my_first_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my_first_app
+//
+//  Created by Алексей  Шевченко on 09.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
